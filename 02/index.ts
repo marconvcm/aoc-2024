9b009f8cd5027df748561abf138de84b362e6a47
Marcos Vinícius Marcon <marconvcm@gmail.com>
2024-12-03 18:50:46 +0100

    Day 02 code cleanup

--- 02/index.ts
+++ 02/index.ts
@@ -32,42 +32,21 @@ function isSafeReport(report: number[]): boolean {
    return false;
 }
 
+export function isSafeReportLine(reportLine: number[]): boolean {
 
-function adjacentPairs(data: number[]): number[][] {
-   return data.slice(0, -1).map((value, index) => [value, data[index + 1]]);
-}
-
-function isSafeReport2(reportSheet: number[]): boolean {
-
-   if (isSafeReport(reportSheet)) {
+   if (isSafeReport(reportLine)) {
       return true;
    }
 
-   const pairs = adjacentPairs(reportSheet);
-
-   const firstNonInc = pairs.findIndex(([a, b]) => !diffOk(a, b, 1));
-   const firstNonDec = pairs.findIndex(([a, b]) => !diffOk(a, b, -1));
-
-   const checks = [firstNonInc, firstNonDec].filter((index) => index !== -1);
-
-   for (const check of checks) {
-       for (const index of [check, check + 1]) {
-           if (index < 0 || index >= reportSheet.length) continue;
-           const newData = [...reportSheet];
-           newData.splice(index, 1);
-           const newReport = newData
-           if (isSafeReport(newReport)) {
-               return true;
-           }
-       }
+   for (let i = 0; i < reportLine.length; i++) {
+      const newReportLine = [...reportLine];
+      newReportLine.splice(i, 1);
+      if (isSafeReport(newReportLine)) {
+         return true;
+      }
    }
 
    return false;
-}
-
-export function diffOk(a: number, b: number, expectedSign: number): boolean {
-   const diff = a - b;
-   return Math.sign(diff) === expectedSign && Math.abs(diff) >= 1 && Math.abs(diff) <= 3;
 }
 
 export function part1(input: string): string {
@@ -78,6 +57,6 @@ export function part1(input: string): string {
 
 export function part2(input: string): string {
    let matrix = readMatrix(input);
-   let safeReports = matrix.filter(isSafeReport2);
+   let safeReports = matrix.filter(isSafeReportLine);
    return `${safeReports.length}`;
 }
