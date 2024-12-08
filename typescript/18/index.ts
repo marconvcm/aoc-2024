901605e1703fdb13c31c958b9d077370db924ac4
Marcos Vinícius Marcon <marconvcm@gmail.com>
2024-12-08 09:31:09 +0100

    remove rust

--- typescript/18/index.ts
+++
@@ -1,7 +0,0 @@
-export function part1(input: string): string {
-   return "";
-}
-
-export function part2(input: string): string {
-   return "";
-}
--- typescript/README.md
+++
@@ -1,2 +0,0 @@
-# aoc-2024
-https://adventofcode.com/2024
